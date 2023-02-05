e195ad6e75476d608df2c197d0f61fe41989d071
Nikita Tyunev <tyunev.nik89@gmail.com>
2023-02-05 23:26:15 +0300

    add testing library and update test env

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^./style.css$': 'identity-obj-proxy',
+    '^components': '<rootDir>/src/components',
+    '^core': '<rootDir>/src/core',
+    '^store': '<rootDir>/src',
+    '^pages': '<rootDir>/src/pages',
+    '^utils': '<rootDir>/src/utils/$1',
+    '^assets': '<rootDir>/src/assets',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
