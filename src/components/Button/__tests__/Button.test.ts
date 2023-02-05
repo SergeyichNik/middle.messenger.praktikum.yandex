ec81c094c07ec1ac8d429aa9b5e48ade58c943eb
Nikita Tyunev <tyunev.nik89@gmail.com>
2023-02-05 23:30:48 +0300

    add unit test for Button component

---
+++ src/components/Button/__tests__/Button.test.ts
@@ -0,0 +1,40 @@
+import { getByRole } from '@testing-library/dom';
+import { renderBlock } from '../../../tests/renderBlock';
+import { Button } from '../Button';
+
+describe('Button', () => {
+  test('should render', async () => {
+    renderBlock(Button, { label: 'test' });
+
+    const button = getByRole(document.body, 'button');
+
+    expect(button).toBeInTheDocument();
+  });
+
+  test('should call onClick', async () => {
+    const mockFn = jest.fn();
+    renderBlock(Button, { onClick: mockFn });
+
+    const button = getByRole(document.body, 'button');
+
+    button.click();
+
+    expect(mockFn).toBeCalled();
+  });
+
+  test('should have text label', async () => {
+    renderBlock(Button, { label: 'test' });
+
+    const button = getByRole(document.body, 'button');
+
+    expect(button).toHaveTextContent(/test/i);
+  });
+
+  test('should be disabled', async () => {
+    renderBlock(Button, { label: 'test', disabled: true });
+
+    const button = getByRole(document.body, 'button');
+
+    expect(button).toBeDisabled();
+  });
+});
